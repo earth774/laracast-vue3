e6a8d3ba7190e29c93091071f478a9d0dd8bcc12
earth774 <sutthiphongnuanma@gmail.com>
2023-11-05 21:09:14 +0700

    Named Slots

--- js/components/App.js
+++ js/components/App.js
@@ -1,9 +1,25 @@
 import Assignments from "./Assignments.js";
+import Panel from "./Panel.js";
 
 export default {
-    components: { Assignments },
+  components: { Assignments,Panel },
 
-    template: `
-        <assignments></assignments>    
+  template: `
+        <div>
+            <assignments>  
+            </assignments>    
+
+            <Panel class="mt-4 w-70">
+                <template v-slot:heading>
+                    Heading
+                </template>
+
+                    test
+                    
+                <template v-slot:footer>
+                    Footer
+                </template>
+            </Panel>
+        </div>
     `,
-}
+};
